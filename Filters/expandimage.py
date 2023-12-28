e76fab0d1e4650bbee83676a39409b9c8ca8f6bf
Julian Andres Mella Valdes <juliaame@ulrik.uio.no>
2023-12-28 10:26:19 +0100

    Refactored expand image into its separate file as its a general method that has multiple applications

---
+++ Filters/expandimage.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+# Expand image in accordance to the kernel matrix size.
+def expand_image(image: np.array, kernel_matrix_size: int) -> np.array:
+    expansion_length = kernel_matrix_size // 2 + 1
+    padded_image = image
+    width = image.shape[0]
+    height = image.shape[1]
+
+    left_wall = np.fliplr(image[0 : , 0 : expansion_length])
+    padded_image = np.concatenate((left_wall, padded_image), axis=1)
+
+    right_wall = np.fliplr(image[0 : , width - expansion_length : width])
+    padded_image = np.concatenate((padded_image, right_wall), axis=1)
+
+    top_left_corner = np.fliplr(image[0 : expansion_length, 0 : expansion_length])
+    top_right_corner = np.fliplr(image[0 : expansion_length, width - expansion_length : width])
+    top_row = np.concatenate((np.concatenate((top_left_corner, image[0 : expansion_length, 0 : ]), axis=1), top_right_corner), axis=1)
+    top_row = np.flipud(top_row)
+
+    padded_image = np.concatenate((top_row, padded_image), axis=0)
+
+    bottom_left_corner = np.fliplr(image[height - expansion_length : height, 0 : expansion_length])
+    bottom_right_corner = np.fliplr(image[height - expansion_length : height, width - expansion_length : width])
+    bottom_row = np.concatenate((np.concatenate((bottom_left_corner, image[height - expansion_length : height, 0 : ]), axis=1), bottom_right_corner), axis=1)
+
+    padded_image = np.concatenate((padded_image, bottom_row), axis=0)
+    return padded_image
--- Filters/gaussianblur.py
+++ Filters/gaussianblur.py
@@ -1,58 +1,8 @@
-import math
-import numpy as np
+from kernelmatrix import calculate_kernel_matrix
+from expandimage import expand_image
 from PIL import Image
+import numpy as np
 import sys
-
-#Calculate the kernel matrix
-def calculate_kernel_matrix(kernel_width: int, radius : int) -> np.array:
-    sigma = max(float(radius / 2) , 1)
-    kernel = [[0.0 for _ in range(kernel_width)] for _ in range(kernel_width)]
-
-    sum = 0
-    for x in range(-radius, radius):
-        for y in range(-radius, radius):
-            exponentNumerator = float(-(x * x + y * y))
-            exponentDenominator = (2 * sigma * sigma)
-
-            eExpression = math.pow(math.e, exponentNumerator / exponentDenominator)
-            kernelValue = (eExpression / (2 * math.pi * sigma * sigma))
-            kernel[x + radius][y + radius] = kernelValue
-            sum += kernelValue
-
-    for x in range (0, kernel_width):
-        for y in range(0, kernel_width):
-            kernel[x][y] /= sum
-
-    kernel = np.reshape(kernel, (kernel_width, kernel_width, 1))
-
-    return kernel
-
-# Expand image in accordance to the kernel matrix size.
-def expand_image(image: np.array, kernel_matrix_size: int) -> np.array:
-    expansion_length = kernel_matrix_size // 2 + 1
-    padded_image = image
-    width = image.shape[0]
-    height = image.shape[1]
-
-    left_wall = np.fliplr(image[0 : , 0 : expansion_length])
-    padded_image = np.concatenate((left_wall, padded_image), axis=1)
-
-    right_wall = np.fliplr(image[0 : , width - expansion_length : width])
-    padded_image = np.concatenate((padded_image, right_wall), axis=1)
-
-    top_left_corner = np.fliplr(image[0 : expansion_length, 0 : expansion_length])
-    top_right_corner = np.fliplr(image[0 : expansion_length, width - expansion_length : width])
-    top_row = np.concatenate((np.concatenate((top_left_corner, image[0 : expansion_length, 0 : ]), axis=1), top_right_corner), axis=1)
-    top_row = np.flipud(top_row)
-
-    padded_image = np.concatenate((top_row, padded_image), axis=0)
-
-    bottom_left_corner = np.fliplr(image[height - expansion_length : height, 0 : expansion_length])
-    bottom_right_corner = np.fliplr(image[height - expansion_length : height, width - expansion_length : width])
-    bottom_row = np.concatenate((np.concatenate((bottom_left_corner, image[height - expansion_length : height, 0 : ]), axis=1), bottom_right_corner), axis=1)
-
-    padded_image = np.concatenate((padded_image, bottom_row), axis=0)
-    return padded_image
 
 #Place the center of the kernel at each pixel of the image
 #Multiply the kernel matrix values with the corresponding pixel values in the local neighbourhood
@@ -62,7 +12,7 @@ def gaussian_blur(image: np.array, expanded_image: np.array, kernel_matrix: np.a
     #Set transparency level of PNG image to max
     if image.shape[2] == 4:
         image[:,:,3:4:1] = 255
-    """
+
     for row in range(0, image.shape[1]):  # Rows
         for column in range(0, image.shape[0]):  # Columns
             # Define the neighborhood region around the current pixel
@@ -79,45 +29,15 @@ def gaussian_blur(image: np.array, expanded_image: np.array, kernel_matrix: np.a
             image[column][row][0] = red_sum
             image[column][row][1] = green_sum
             image[column][row][2] = blue_sum
-    """
 
-    # Extract dimensions and channels of the image
-    image_height, image_width, _ = image.shape
-
-    # Calculate the radius of the kernel matrix
-    kernel_matrix_radius = kernel_matrix_size // 2
-
-    # Generate indices for the sliding windows
-    rows, cols = np.indices((image_width, image_height))
-
-    # Define the sliding window indices by adding offsets based on kernel matrix radius
-    row_offsets = np.arange(-kernel_matrix_radius, kernel_matrix_radius + 1)
-    col_offsets = np.arange(-kernel_matrix_radius, kernel_matrix_radius + 1)
-
-    # Calculate indices for the neighborhoods
-    neighborhood_rows = rows.reshape(-1, 1, 1) + row_offsets
-    neighborhood_cols = cols.reshape(-1, 1, 1) + col_offsets
-
-    # Extract neighborhood regions using advanced indexing from the provided expanded_image
-    neighborhood_red = expanded_image[neighborhood_cols, neighborhood_rows, 0]
-    neighborhood_green = expanded_image[neighborhood_cols, neighborhood_rows, 1]
-    neighborhood_blue = expanded_image[neighborhood_cols, neighborhood_rows, 2]
-
-    # Perform element-wise multiplication with the kernel matrix
-    red_sums = np.sum(neighborhood_red * kernel_matrix, axis=(2, 3))
-    green_sums = np.sum(neighborhood_green * kernel_matrix, axis=(2, 3))
-    blue_sums = np.sum(neighborhood_blue * kernel_matrix, axis=(2, 3))
-
-# Assign the summed values to the corresponding pixel channels after normalization if needed
-    image[:, :, 0:1] = red_sums
-    image[:, :, 1:2] = green_sums
-    image[:, :, 2:3] = blue_sums
     return image.astype("uint8")
 
 def gaussian_filter(image: np.array, radius: int) -> np.array:
     kernel_width = (2 * radius) + 1
-    kernel_matrix = calculate_kernel_matrix(kernel_width, radius)
+
+    kernel_matrix = calculate_kernel_matrix(radius, kernel_width)
     expanded_image = expand_image(image, kernel_width)
+
     blurred_image = gaussian_blur(np.zeros_like(image), expanded_image, kernel_matrix, kernel_width)
     return blurred_image
 
