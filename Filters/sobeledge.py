7148ce6b2ac5e0e166cd24c321f818d66afac88c
Julian Andres Mella Valdes <juliaame@ulrik.uio.no>
2023-12-29 01:51:48 +0100

    Refactored sobel convolution aswell, potentially add separate functions for horizontal and vertical convolution only

--- Filters/sobeledge.py
+++ Filters/sobeledge.py
@@ -31,50 +31,11 @@ def sobel_convolution(image : np.array, colored : bool) -> np.array:
         sobel_filtered_image[:,:,3:4] = 255
     Image.fromarray(sobel_filtered_image).show()
     return image
-"""
-def magnitude_of_edge(vertical : np.array, horizontal : np.array) -> np.array:
-    magnitude = np.zeros_like(vertical)
 
-    for row in range(image.shape[0]):
-        for column in range(image.shape[1]):
-            magnitude_value =
-    for row in range(0, magnitude.shape[1]):  # Rows
-        for column in range(0, magnitude.shape[0]):  # Columns
-            magnitude_value = math.sqrt((vertical[column][row][0] ** 2) + (horizontal[column][row][0] ** 2))
-            magnitude[column][row][0] = magnitude_value
-            magnitude[column][row][1] = magnitude_value
-            magnitude[column][row][2] = magnitude_value
-
-    if magnitude.shape[2] == 4:
-        magnitude[:,:,3:4] = 255
-
-
-    #for row in range(0, magnitude.shape[1]):  # Rows
-    #    for column in range(0, magnitude.shape[0]):  # Columns
-
-    #        magnitude[column][row][0] = magnitude[column][row][0] * 0.5
-     #       magnitude[column][row][1] = magnitude[column][row][0] * 0.5
-     #       magnitude[column][row][2] = magnitude[column][row][0] * 0.5
-
-    magnitude = magnitude.astype("uint8")
-    Image.fromarray(magnitude).show()
-"""
 def sobel_edge_detector(image : np.array):
     sobel_convolution(image, True)
-    #testh = sobel_convolution(np.zeros_like(image), expand_image(blurred, 3), horizontal_kernel, 3)
-
-
-
-    #testv = sobel_convolution(np.zeros_like(image), expand_image(blurred, 3), vertical_kernel, 3)
-
-
-
-    #magnitude_of_edge(testv, testh)
 
 
 if __name__ == "__main__":
     image = np.asarray(Image.open("../Valve.png"))
     sobel_edge_detector(image)
-    #test = np.array([[[ 157, 0, -157],[348,0,-348],[191,0,-191]],[[ 155,    0, -155],[ 346,0, -346],[ 191,0, -191]],[[ 154,    0, -154],[ 344,0, -344],[ 190,0,-190]]])
-    #print(np.sum(test))
-    #Image.fromarray(test).show()
